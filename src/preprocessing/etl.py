322e1ff3c1ef3ace9c7de5f514c5da2b9aace32d
Longarini <l.longarini@overside.it>
2024-01-23 09:36:22 +0100

    rename, modifiche al main e al preprocessing, refactoring

--- src/preprocessing/etl.py
+++ src/preprocessing/etl.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
 from src.utils import constants as cs
+from datetime import datetime
 
 # merge all files in specific folder
-def merge_files (data_dir: str = 'src/dataset/CS2_38', file_name:str = 'CS2_38'):
+def merge_files (data_dir: str = 'src/dataset/CS2_38', file_name:str = 'CS2_38', is_charge = True):
     # define new data frame
     cleaned = pd.DataFrame()
+    
     # consider only channel sheets
     for filename in os.listdir(data_dir):
         xls_file = pd.ExcelFile(os.path.join(data_dir, filename))
@@ -20,4 +22,22 @@ def merge_files (data_dir: str = 'src/dataset/CS2_38', file_name:str = 'CS2_38')
             df[cs.c_index] += max_cindex
             # concat all files 
             cleaned = pd.concat([cleaned, df], ignore_index=True)
-            cleaned.to_csv(os.path.join(cs.ds_cleaned, f'{file_name}.csv'), index=False)
+            if is_charge: 
+                # consider only step index 2-4
+                cleaned = cleaned[~cleaned[cs.step_index].isin(cs.charge_indexes)]
+                # check current
+                cond_sindex = (cleaned[cs.step_index] == 4)
+                cond_curr = (cleaned[cs.current] > 0.55)
+                # reduce dataframe
+                cleaned = cleaned[~(cond_sindex & cond_curr)]
+                # for key, group in cleaned.groupby(cs.c_index):
+                #     diffs =  group[cs.current].diff().abs() 
+                #     mask = ~(diffs > 0.0502)
+                #     cleaned.loc[cleaned[cs.c_index] == key, :] = cleaned.loc[cleaned[cs.c_index] == key, :][mask]
+                cleaned = cleaned[cs.charge_list]
+                cleaned.to_csv(os.path.join(cs.ds_cleaned, f'charge-{file_name}'), index=False)    
+            else:
+                # consider only step index 7
+                # cleaned = cleaned[~cleaned[cs.step_index].isin(cs.discharge_indexes)]
+                cleaned = cleaned[cs.discharge_list]
+                cleaned.to_csv(os.path.join(cs.ds_cleaned, f'discharge-{file_name}'), index=False)    
