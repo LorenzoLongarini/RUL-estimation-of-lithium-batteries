a3ac039f96397190a26341339daf564aa4581372
Longarini <l.longarini@overside.it>
2024-01-30 17:08:50 +0100

    inizialize training

--- src/preprocessing/hi.py
+++ src/preprocessing/hi.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from src.utils import constants as cs
 import numpy as np
+from scipy.integrate import simps
 
 def make_hi(path, file_name):
     hiv = pd.DataFrame()
@@ -15,23 +16,20 @@ def make_hi(path, file_name):
     df_hiv = df_hiv[(df_hiv[cs.voltage] >= 3.8) & (df_hiv[cs.voltage] <= 4.2001)]
 
     for key, group in df_hiv.groupby(cs.c_index):
-        hi_v = np.trapz(group[cs.voltage], x=group[cs.test_time])
-        hi_v_hour = hi_v / 3600.0
+        hi_v = simps(group[cs.voltage], x=group[cs.test_time]/3600.0)
         cycle = group[cs.c_index].iloc[0]
 
-        row = pd.DataFrame([{'hi_v': hi_v_hour, 'cycle': cycle}])
+        row = pd.DataFrame([{'hi_v': hi_v, 'cycle': cycle}])
         hiv = pd.concat([hiv, row])
 
     for key, group in df_hii.groupby(cs.c_index):
-        hi_i = np.trapz(group[cs.current], x=group[cs.test_time])
-        hi_i_hour = hi_i / 3600.0
+        hi_i = simps(group[cs.current], x=group[cs.test_time]/3600)
         cycle = group[cs.c_index].iloc[0]
-
         # Aggiungi la colonna hi_i solo se è presente nel DataFrame
         # if 'hi_i' in hi.columns:
         #     hi.loc[hi['cycle'] == cycle, 'hi_i'] = hi_i_hour
         # else:
-        row = pd.DataFrame([{'hi_i': hi_i_hour, 'cycle': cycle}])
+        row = pd.DataFrame([{'hi_i': hi_i, 'cycle': cycle}])
         hii = pd.concat([hii, row])
 
     # Riempi eventuali valori mancanti con 0
