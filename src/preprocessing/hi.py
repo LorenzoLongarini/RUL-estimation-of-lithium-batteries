aa596b7bc5b872caaf930d5b42a9abe30a1f0e05
Longarini <l.longarini@overside.it>
2024-01-31 17:43:41 +0100

    refactoring

--- src/preprocessing/hi.py
+++ src/preprocessing/hi.py
@@ -5,9 +5,11 @@ import numpy as np
 from scipy.integrate import simps
 
 def make_hi(path, file_name):
+    #initialize dataframes
     hiv = pd.DataFrame()
     hii = pd.DataFrame()
 
+    #define different df for hiv and hii
     df_hiv = pd.read_csv(path)
     df_hiv[cs.index] = df_hiv.index
     df_hii = df_hiv
@@ -15,6 +17,7 @@ def make_hi(path, file_name):
     df_hii = df_hii[(df_hii[cs.step_index] == 4)]
     df_hiv = df_hiv[(df_hiv[cs.voltage] >= 3.8) & (df_hiv[cs.voltage] <= 4.2001)]
 
+    #integrate voltage through time and convert into Voltage/hour
     for key, group in df_hiv.groupby(cs.c_index):
         hi_v = simps(group[cs.voltage], x=group[cs.test_time]/3600.0)
         cycle = group[cs.c_index].iloc[0]
@@ -22,18 +25,12 @@ def make_hi(path, file_name):
         row = pd.DataFrame([{'hi_v': hi_v, 'cycle': cycle}])
         hiv = pd.concat([hiv, row])
 
+    #integrate current through time and convert into Current/hour
     for key, group in df_hii.groupby(cs.c_index):
         hi_i = simps(group[cs.current], x=group[cs.test_time]/3600)
         cycle = group[cs.c_index].iloc[0]
-        # Aggiungi la colonna hi_i solo se è presente nel DataFrame
-        # if 'hi_i' in hi.columns:
-        #     hi.loc[hi['cycle'] == cycle, 'hi_i'] = hi_i_hour
-        # else:
         row = pd.DataFrame([{'hi_i': hi_i, 'cycle': cycle}])
         hii = pd.concat([hii, row])
-
-    # Riempi eventuali valori mancanti con 0
-    # hi = hi.fillna(0)
 
     # Salva il DataFrame come CSV senza l'indice
     hiv.to_csv(f'{cs.ds_hi}/hiv-{file_name}', index=False)
